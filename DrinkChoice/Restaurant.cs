9afd86586b4556f4e99f38e3cc1e10761fc589fc
bkleach011 <bkleach01@gmail.com>
2025-10-28 19:10:27 -0500

    Finished Programmatic Controls Tutorial

--- DrinkChoice/Restaurant.cs
+++ DrinkChoice/Restaurant.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace DrinkChoice
 {
-    public class Restaurant 
+    public class Restaurant : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public string Name { get; init; }
 
         private List<SodaChoice> _possibleSodas = new List<SodaChoice>();
+
         public List<SodaChoice> PossibleSodas => _possibleSodas;
+
+        public void OnChosenChange(object? sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NumChosen)));
+        }
 
         public Restaurant(string n)
         {
@@ -21,7 +29,8 @@ namespace DrinkChoice
             foreach (SodaType soda in Enum.GetValues(typeof(SodaType)))
             {
                 SodaChoice choice = new SodaChoice(soda);
-                PossibleSodas.Add(new SodaChoice(soda));
+                choice.PropertyChanged += OnChosenChange;
+                PossibleSodas.Add(choice);
             }
         }
 
